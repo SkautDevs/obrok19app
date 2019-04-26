355b04a57f4fef974517a3a29cca894db4000210
Lung <lung@skaut.cz>
2019-04-26 23:10:35 +0200

    added programs content + added bit of styling + fixed map size

--- src/routes.php
+++ src/routes.php
@@ -13,7 +13,23 @@ $app->get('/mapa', function(Request $request, Response $response, array $args) {
 })->setName('map');
 
 $app->get('/programy', function(Request $request, Response $response, array $args) {
-	return $this->view->render($response, 'programs.twig', $args);
+	$programs = array_fill(0, 4, [
+		'name' => 'Přednáška Franty Říhy o plážových povalečích',
+		'perex' => 'Je to o plážích a Lorem Ipsum Dolor Sit Amet...',  // null
+		'startDatetime' => (new \DateTime())->format('d. m. H:i'),
+		'endDatetime' => (new \DateTime('now + 2 hours'))->format('d. m. H:i'),
+		'place' => 'zadní louka', // null
+		'bringAlong' => 'písek a lehátka',  // null
+		'lector' => 'Franta Říha',
+		'capacity' => 23,
+		'seatsTaken' => 22,
+	]);
+	$dummyPorgrams = json_encode(array_fill(0, 2, [
+			'sectionName' => 'Vapro blok 2',
+			'programs' => $programs
+		]
+	));
+	return $this->view->render($response, 'programs.twig', ['sections' => json_decode($dummyPorgrams, true)]);
 })->setName('programs');
 
 $app->get('/odkazy', function(Request $request, Response $response, array $args) {
